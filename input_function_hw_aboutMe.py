220fb30206d40a1b40e14bff6774d8cb831a5599
NishanthMekala <104415101+NishanthMekala@users.noreply.github.com>
2022-05-04 14:18:37 +0530

    Add files via upload

---
+++ input_function_hw_aboutMe.py
@@ -0,0 +1,8 @@
+name=input('Enter your Name: ')#str
+scname=input('Enter your School Name: ')#str
+rno=input('Enter your Roll no.: ')#str
+p= input('Enter your Perentage: ')#float
+h=float(input('Enter your height: '))#float
+m=input('Enter your favourite Movie: ')#str
+
+print('My name is', name, 'school name is', scname, 'Roll no.:', rno,  '\nPercentage is', p+'%', "Height is", h, 'My favourite movie is', m)
